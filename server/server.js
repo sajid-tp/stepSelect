a10facd3f597715932cfe4fecdfd3ec89694bed9
Sajid T P <sajidtp274@gmail.com>
2026-09-09 10:27:59 +0530

    Set up

---
+++ server/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config(); 
+const express = require('express'); 
+const cors = require('cors'); 
+const connectDB = require('./src/config/db'); 
+const app = express(); 
+app.use(cors()); 
+app.use(express.json()); 
+connectDB(); 
+app.get('/', (req, res) => { res.send('stepSelect API is running'); }); 
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
